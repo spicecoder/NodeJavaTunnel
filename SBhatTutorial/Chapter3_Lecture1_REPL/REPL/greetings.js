e25e1d42a04ea3f6d1f5b5a6e4c674023d6a8515
unknown <921639@tcsauslt425.TCSAUS.tcs.com>
2015-03-09 15:32:25 +1100

    cleanuo nodejavatunnel

--- SBhatTutorial/Chapter3_Lecture1_REPL/REPL/greetings.js
+++
@@ -1,3 +0,0 @@
-function hello(fullname){
-	console.log("Hi " + fullname + ". The time is : " + new Date().getHours() + " Hours and " + new Date().getMinutes() + " minutes !");
-}
--- SBhatTutorial/Chapter3_Lecture5_QRGen/QRGen/app.js
+++
@@ -1,8 +0,0 @@
-var qrImage = require('qr-image');
-var fs = require('fs');
-
-qrImage
-	.image("http://www.nodejs.org", {type:'png', size:20})
-	.pipe(fs.createWriteStream("MyQRCode.png"));
-	
-console.log('QR Code Generated !');
